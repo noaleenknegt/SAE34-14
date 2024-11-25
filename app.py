96cbdb64e3b97e973dd06261802c77a302ae0d27
Natow13 <Noa.leenknegt@edu.univ-fcomte.fr>
2024-11-25 09:36:08 +0100

    rajout sql

--- app.py
+++ app.py
@@ -4,6 +4,30 @@ app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.secret_key = 'random token'
 
+from flask import session, g
+import pymysql.cursors
+
+def get_db():
+    if 'db' not in g:
+        g.db =  pymysql.connect(
+            host="serveurmysql",
+            user="nleenkne",
+            password="mdp",
+            database="BDD_nleenkne",
+            charset='utf8mb4',
+            cursorclass=pymysql.cursors.DictCursor
+        )
+    return g.db
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+@app.route('/client', methods=['GET'])
+def show_client():
+    return render_template('client.html')
 
 @app.route('/', methods=['GET'])
 def show_layout():
