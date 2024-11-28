b515b110380f2ba33ce3f830c1b097c7d4b80f75
Natow13 <Noa.leenknegt@edu.univ-fcomte.fr>
2024-11-28 16:53:18 +0100

    aaa

--- app.py
+++ app.py
@@ -28,11 +28,17 @@ def teardown_db(exception):
 @app.route('/client/show', methods=['GET'])
 def show_client():
     my_cursor = get_db().cursor()
+    rang = {};
     sql = """SELECT * FROM Client"""
     my_cursor.execute(sql)
     clients = my_cursor.fetchall()
+    sql = """SELECT * FROM Rang"""
+    my_cursor.execute(sql)
+    rangs = my_cursor.fetchall()
+    for i in rangs:
+        rang[i['IdRang']] = i['LibelleRang']
     get_db().commit()
-    return render_template('client/show_client.html', clients=clients)
+    return render_template('client/show_client.html', clients=clients, rang=rang)
 
 
 @app.route('/client/delete', methods=['GET'])
@@ -44,10 +50,25 @@ def delete_client():
     get_db().commit()
     return redirect("/client/show")
 
-@app.route('/client/add', methods=['GET'])
+@app.route('/client/edit', methods=['GET'])
 def edit_client():
-    id = request.args.get('id')
-    return render_template('client/edit_client.html', id=id)
+    my_cursor = get_db().cursor()
+    sql = """SELECT * FROM Client WHERE IdClient=%s"""
+    my_cursor.execute(sql, (request.args.get('id')))
+    client = my_cursor.fetchone()
+    sql = """SELECT * FROM Rang"""
+    my_cursor.execute(sql)
+    rangs = my_cursor.fetchall()
+    get_db().commit()
+    return render_template('client/edit_client.html', client=client, rangs=rangs)
+
+@app.route('/client/edit', methods=['POST'])
+def valid_edit_client():
+    my_cursor = get_db().cursor()
+    sql = """UPDATE Client SET Nom=%s, Prenom=%s, IdRang=%s, Telephone=%s WHERE IdClient=%s"""
+    my_cursor.execute(sql, (request.form['Nom'], request.form['Prenom'], request.form['IdRang'],request.form['Telephone'], request.form['IdClient']))
+    get_db().commit()
+    return redirect("/client/show")
 
 @app.route('/reduction', methods=['GET'])
 def show_reduction():
